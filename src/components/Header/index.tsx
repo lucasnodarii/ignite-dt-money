878de49f739c920e503e72cb32044df340544272
Lucas Nodari <lucasbnodari@gmail.com>
2023-08-01 16:55:21 -0300

    feat: new transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import igniteLogo from '../../assets/logo-ignite.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
 export function Header() {
@@ -7,7 +9,12 @@ export function Header() {
       <HeaderContent>
         <img src={igniteLogo} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
